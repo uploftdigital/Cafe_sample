7ffc5567f9c2e617e7fe084984a9695366159c0e
Mohit <mohitramesh14@gmail.com>
2026-03-30 00:00:35 +0530

    Your commit message

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from "@vercel/analytics/next"
 import type { Metadata, Viewport } from 'next'
 import { Inter, Playfair_Display } from 'next/font/google'
 import { Analytics } from '@vercel/analytics/next'
